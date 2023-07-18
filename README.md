b9cc9a06180017ec4aaaa3531ffe6d98cb6147d8
Omar Ibrahim <mswellam@geekyair.com>
2023-07-18 18:28:40 +0300

    change readme file

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # tms
 Task Management System
+
+# Installation
+1 - git clone https://github.com/dev-omaribrahim/tms.git
+
+2 - cd tms
+
+3 - create virtualenv & activate it
+
+4 - pip install -r requirements.txt
+
+5 - python manage.py makemigrations
+
+6 - python manage.py migrate
+
+7 - python manage.py createsuperuser
+
+
+# run
+
+1 - python manage.py runserver
--- tms/urls.py
+++ tms/urls.py
@@ -7,12 +7,10 @@ from rest_framework import permissions
 # Create a schema view for API documentation
 schema_view = get_schema_view(
     openapi.Info(
-        title="School Management System API",
+        title="Task Management System",
         default_version="v1",
-        description="API Documentation for School management System",
+        description="API Documentation for Task Management System",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
